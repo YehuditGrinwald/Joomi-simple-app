852cb50eb5a08b1d7d9820a4b2aa9a2eace805d1
100yehudit <1000yehudit@gmail.com>
2017-03-05 15:16:05 +0200

    link to folder js

---
+++ Scripts/App.js
@@ -0,0 +1,37 @@
+// JavaScript source code
+
+    //function validForm() {
+    //    validPhone();
+    //    ValidateEmail();
+    //}
+
+        function validPhone() //Function for validating phone
+        {
+            var PhoneNumber = document.getElementById('phone').value;
+            var Phoneno = /^\d{10}$/;
+            if ((Phoneno.test(PhoneNumber)))
+            { return true; }
+            else
+            {
+                alert("Not a valid Phone Number");
+                return false;
+            }
+        }
+
+function ValidateEmail()//Function for validating email
+{
+    var ImailAddress = document.getElementById('email').value;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (mailformat.test(ImailAddress))
+    {
+        alert("You have valid email address!");
+
+        return true;
+    }
+    else
+    {
+        alert("You have entered an invalid email address!");
+        return false;
+    }
+}
+
