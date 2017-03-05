4614670c77ff3265bae195635d027cc7544381f7
100yehudit <1000yehudit@gmail.com>
2017-03-05 21:42:13 +0200

    שינויים

--- Scripts/App.js
+++ Scripts/App.js
@@ -35,3 +35,64 @@ function ValidateEmail()//Function for validating email
     }
 }
 
+
+function scrollWin(elementId) {
+    window.scrollDowen(400,400);
+}
+
+function scroll_to(divelementId) {
+    if (elementId.scrollTop < elementId.scrollHeight - elementId.clientHeight)
+        elementId.scrollTop += 10; // move down
+              }
+var smoothScroll = function (elementId) {
+    var MIN_PIXELS_PER_STEP = 16;
+    var MAX_SCROLL_STEPS = 30;
+    var scrollContainer = target;
+    do {
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do {
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    var pixelsPerStep = Math.max(MIN_PIXELS_PER_STEP,
+                                 (targetY - scrollContainer.scrollTop) / MAX_SCROLL_STEPS);
+
+    var stepFunc = function () {
+        scrollContainer.scrollTop =
+            Math.min(targetY, pixelsPerStep + scrollContainer.scrollTop);
+
+        if (scrollContainer.scrollTop >= targetY) {
+            return;
+        }
+
+        window.requestAnimationFrame(stepFunc);
+    };
+
+    window.requestAnimationFrame(stepFunc);
+}
+
+
+
+//$(document).ready(function(){
+
+//    /** 
+//     * This part does the "fixed navigation after scroll" functionality
+//     * We use the jQuery function scroll() to recalculate our variables as the 
+//     * page is scrolled/
+//     */
+//    $(window).scroll(function(){
+//        var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
+//        var div_top = $('#nav-anchor').offset().top;
+//        if (window_top > div_top) {
+//            $('nav').addClass('stick');
+//        } else {
+//            $('nav').removeClass('stick');
+//        }
+//    });
+
