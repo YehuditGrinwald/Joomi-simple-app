63df42efa424fd5a230104ce3a17e27dcf7c08ea
100yehudit <1000yehudit@gmail.com>
2017-03-06 17:12:28 +0200

    דshow normal form

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,9 +1,6 @@
 // JavaScript source code
 
-    //function validForm() {
-    //    validPhone();
-    //    ValidateEmail();
-    //}
+    
 	smoothScroll.init();
 
         function validPhone() //Function for validating phone
@@ -78,22 +75,4 @@ var smoothScroll = function (elementId) {
     window.requestAnimationFrame(stepFunc);
 }
 
-
-
-//$(document).ready(function(){
-
-//    /** 
-//     * This part does the "fixed navigation after scroll" functionality
-//     * We use the jQuery function scroll() to recalculate our variables as the 
-//     * page is scrolled/
-//     */
-//    $(window).scroll(function(){
-//        var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
-//        var div_top = $('#nav-anchor').offset().top;
-//        if (window_top > div_top) {
-//            $('nav').addClass('stick');
-//        } else {
-//            $('nav').removeClass('stick');
-//        }
-//    });
 
