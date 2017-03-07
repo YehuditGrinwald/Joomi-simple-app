fe9e8143f97005f77604eb3c7f22fae037ec6b11
100yehudit <1000yehudit@gmail.com>
2017-03-07 11:04:25 +0200

    consider kod2

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 
-    
 	smoothScroll.init();
+	
 $(document).ready(function() {
 		$('#detailes').on('submit', function() {
 			
@@ -21,38 +21,6 @@ $(document).ready(function() {
   return isValid  
 });
 });
-
-
-        function validPhone() //Function for validating phone
-        {
-            var PhoneNumber = document.getElementById('phone').value;
-            var Phoneno = /^\d{10}$/;
-            if ((Phoneno.test(PhoneNumber)))
-            { return true; }
-            else
-            {
-                alert("Not a valid Phone Number");
-                return false;
-            }
-        }
-
-function ValidateEmail()//Function for validating email
-{
-    var ImailAddress = document.getElementById('email').value;
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (mailformat.test(ImailAddress))
-    {
-        alert("You have valid email address!");
-
-        return true;
-    }
-    else
-    {
-        alert("You have entered an invalid email address!");
-        return false;
-    }
-}
-
 
 function scrollWin(elementId) {
     window.scrollDowen(400,400);
