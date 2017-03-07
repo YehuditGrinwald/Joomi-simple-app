5b2a8e3937f5de3bff3aea470ea5ed6a6a2add9d
100yehudit <1000yehudit@gmail.com>
2017-03-07 13:23:33 +0200

    end

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 
-	smoothScroll.init();
-	
+
+//validation inputs
 $(document).ready(function() {
 		$('#detailes').on('submit', function() {
 			
@@ -22,45 +22,6 @@ $(document).ready(function() {
 });
 });
 
-function scrollWin(elementId) {
-    window.scrollDowen(400,400);
-}
-
-function scroll_to(divelementId) {
-    if (elementId.scrollTop < elementId.scrollHeight - elementId.clientHeight)
-        elementId.scrollTop += 10; // move down
-              }
-var smoothScroll = function (elementId) {
-    var MIN_PIXELS_PER_STEP = 16;
-    var MAX_SCROLL_STEPS = 30;
-    var scrollContainer = target;
-    do {
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
-    } while (scrollContainer.scrollTop == 0);
-
-    var targetY = 0;
-    do {
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
-    } while (target = target.offsetParent);
-
-    var pixelsPerStep = Math.max(MIN_PIXELS_PER_STEP,
-                                 (targetY - scrollContainer.scrollTop) / MAX_SCROLL_STEPS);
-
-    var stepFunc = function () {
-        scrollContainer.scrollTop =
-            Math.min(targetY, pixelsPerStep + scrollContainer.scrollTop);
-
-        if (scrollContainer.scrollTop >= targetY) {
-            return;
-        }
-
-        window.requestAnimationFrame(stepFunc);
-    };
-
-    window.requestAnimationFrame(stepFunc);
-}
-
+//scroll Windows
+smoothScroll.init();
 
